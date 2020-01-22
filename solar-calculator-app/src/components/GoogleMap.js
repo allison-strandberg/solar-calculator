bfe262b57e1d59f45eb7358813ebb5b03033ff53
allison-strandberg <allisonstrandberg@gmail.com>
2020-01-22 08:58:42 -0500

    Add an autocompleting search field
    - Add an input field that suggests addresses based on user input.

--- solar-calculator-app/src/components/GoogleMap.js
+++ solar-calculator-app/src/components/GoogleMap.js
@@ -7,39 +7,57 @@ const location = {
   lng: -71.088171,
 };
 
-function GoogleMap(props) {
-    const googleMapRef = React.createRef();
-    const googleMap = useRef(null);
+const GoogleMap = (props) => {
+  const googleMapRef = React.createRef();
+  const googleMap = useRef(null);
+  const searchFieldRef = React.createRef();
+  const searchField = useRef(null);
 
-    const createGoogleMap = () =>
-        new window.google.maps.Map(googleMapRef.current, {
-        		disableDefaultUI: true,
-            zoom: 17,
-            center: {
-                lat: location.lat,
-                lng: location.lng,
-            }
-        });
-
-    // Render the map when the component updates.
-    useEffect(() => {
-        const googleMapScript = document.createElement('script');
-        googleMapScript.src = "https://maps.googleapis.com/maps/api/js?" +
-        	"key=" + GOOGLE_MAPS_API_KEY;
-        window.document.body.appendChild(googleMapScript);
-
-        // Ensure files from Google Maps have loaded before creating the map.
-        googleMapScript.addEventListener('load', () => {
-            googleMap.current = createGoogleMap();
-        })
+  const createGoogleMap = () =>
+    new window.google.maps.Map(googleMapRef.current, {
+      disableDefaultUI: true,
+      zoom: 17,
+      center: {
+        lat: location.lat,
+        lng: location.lng,
+      }
     });
 
-    return (
-        <div
-            className="google-map"
-            ref={googleMapRef}
+  const createAutocompleteField = () =>
+    new window.google.maps.places.Autocomplete(searchFieldRef.current, {
+      types: ['address']
+    });
+
+  // Render the map when the component updates.
+  useEffect(() => {
+    const googleMapScript = document.createElement('script');
+    googleMapScript.src = "https://maps.googleapis.com/maps/api/js?" +
+      "key=" + GOOGLE_MAPS_API_KEY +
+      "&libraries=places";
+    window.document.body.appendChild(googleMapScript);
+
+    // Ensure files from Google Maps have loaded before creating the map.
+    googleMapScript.addEventListener('load', () => {
+      googleMap.current = createGoogleMap();
+      searchField.current = createAutocompleteField();
+    })
+  });
+
+  return (
+    <div className="google-map-wrapper">
+      <div className="panel">
+        <input
+          className="field-search"
+          type="text"
+          ref={searchFieldRef}
         />
-    )
+      </div>
+      <div
+        className="google-map"
+        ref={googleMapRef}
+      />
+    </div>
+  );
 }
 
 export default GoogleMap
